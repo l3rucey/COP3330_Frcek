f8106332408eda3ccb93f5647683c294fcdd4142
l3rucey <l3ruceyb@gmail.com>
2020-09-14 22:33:02 -0400

    minor change

--- frcek_p1/src/Application.java
+++ frcek_p1/src/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
 
         Encrypter myEncrypter = new Encrypter();
-        String encryptedValue = myEncrypter.encrypt("1234");
+        String encryptedValue = myEncrypter.encrypt("5678");
 
         Decrypter myDecrypter = new Decrypter();
         String decryptedValue = myDecrypter.decrypt("0189");
