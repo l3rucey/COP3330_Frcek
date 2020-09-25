9265c3310dc568a3aef7f6bcd3c21eebc25d41c1
l3rucey <l3ruceyb@gmail.com>
2020-09-25 00:41:54 -0400

    initial push

---
+++ frcek_p2/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) {
+        BodyMassIndex bub = new BodyMassIndex(28);
+    }
+}
---
+++ frcek_p2/src/BodyMassIndex.java
@@ -0,0 +1,13 @@
+public class BodyMassIndex {
+    private int bmi;
+
+    public BodyMassIndex(int x) {
+        this.bmi = x;
+        System.out.println(bmi);
+    }
+    /*
+    public static void printDat() {
+        System.out.println(bmi);
+    }
+     */
+}
