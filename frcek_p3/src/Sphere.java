6dfd606b4eed01893899e9f63e2c2d29f9cd92cd
l3rucey <l3ruceyb@gmail.com>
2020-10-12 21:47:17 -0400

    quick check

--- frcek_p3/src/Sphere.java
+++ frcek_p3/src/Sphere.java
@@ -13,8 +13,7 @@ public class Sphere extends Shape3D{
     public double getArea() {
         return 4*Math.PI*Math.pow(radius, 2);
     }
-
-    // Why does changing the (4.0/3.0) to (4/3) give wrong answer?
+    
     public double getVolume() {
         return (4.0/3.0)*Math.PI*Math.pow(radius, 3);
     }
