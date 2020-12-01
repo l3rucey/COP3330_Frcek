41ee15fec445f1d66dab3d8baa3c874f11c53e43
l3rucey <l3ruceyb@gmail.com>
2020-12-01 03:36:53 -0500

    Finished up TaskItem tests

--- frcek_p5/src/TaskItemTest.java
+++ frcek_p5/src/TaskItemTest.java
@@ -36,12 +36,48 @@ public class TaskItemTest {
         assertEquals("new desc", list.getItemList().get(0).getDescription());
     }
 
-//    @Test
-//    public void editingDueDateFailsWithInvalidDateFormat() {
-//
-//    }
-//    editingDueDateFailsWithInvalidYYYMMDD()
-//    editingDueDateSucceedsWithExpectedValue()
-//    editingTitleFailsWithEmptyString()
-//    editingTitleSucceedsWithExpectedValue()
+    @Test
+    public void editingDueDateFailsWithInvalidDateFormat() {
+        TaskList list = new TaskList();
+        list.createList();
+        list.addToList("task 1", "desc", "1997-09-09");
+        list.editList(0, 1,"task 2", "new desc", "08/07/09");
+        assertEquals("1997-09-09", list.getItemList().get(0).getDate());
+    }
+
+    @Test
+    public void editingDueDateFailsWithInvalidYYYMMDD() {
+        TaskList list = new TaskList();
+        list.createList();
+        list.addToList("task 1", "desc", "1997-09-09");
+        list.editList(0, 1,"task 2", "new desc", "08/27/09");
+        assertEquals("1997-09-09", list.getItemList().get(0).getDate());
+    }
+
+    @Test
+    public void editingDueDateSucceedsWithExpectedValue() {
+        TaskList list = new TaskList();
+        list.createList();
+        list.addToList("task 1", "desc", "1997-09-09");
+        list.editList(0, 1,"task 2", "new desc", "2009-08-08");
+        assertEquals("2009-08-08", list.getItemList().get(0).getDate());
+    }
+
+    @Test
+    public void editingTitleFailsWithEmptyString() {
+        TaskList list = new TaskList();
+        list.createList();
+        list.addToList("task 1", "desc", "1997-09-09");
+        list.editList(0, 1,"", "new desc", "2008/27/09");
+        assertEquals(null, list.getItemList().get(0).getTitle());
+    }
+
+    @Test
+    public void editingTitleSucceedsWithExpectedValue() {
+        TaskList list = new TaskList();
+        list.createList();
+        list.addToList("task 1", "desc", "1997-09-09");
+        list.editList(0, 1,"task 2", "new desc", "2008/27/09");
+        assertEquals("task 2", list.getItemList().get(0).getTitle());
+    }
 }
